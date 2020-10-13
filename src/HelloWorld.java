03667147b35ee29218b2fa20d20f93ef3a2efe8a
Medini M Krishnan <u1971335@unimail.hud.ac.uk>
2020-10-13 17:13:03 +0100

    Demo

---
+++ src/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * A demo class
+ * @author Medini Krishnan
+ * @version October 2020
+ */
+public class HelloWorld {
+    /**
+     * A demo method
+     * @param args command line arguments (not used)
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
